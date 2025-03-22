bc9b86e415b3eda43cd98eed0c697a31762f8de7
Jacob-Cahalane <jacobrc05172005@gmail.com>
2025-03-22 15:40:15 -0700

    Formatted output and finalized program

--- array_of_structs.cpp
+++ array_of_structs.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 
 // Jacob
@@ -17,16 +18,14 @@ const int MAX_DAYS = 31;
 
 // Function Prototypes
 void readTemperatures(TemperatureRecord monthTemp[], int& size); // TODO: Fix the parameters
-void printTemperatures(const TemperatureRecord monthTemp[], int size);
-TemperatureRecord findMin(const ???);
-TemperatureRecord findMax(const ???);
-double findAverage(const ???);
+void printTemperatures(ofstream& dataOut, const TemperatureRecord monthTemp[], int size);
+TemperatureRecord findMin(const TemperatureRecord monthTemp[], int size);
+TemperatureRecord findMax(const TemperatureRecord monthTemp[], int size); 
+double findAverage(const TemperatureRecord monthTemp[], int size);
 
 int main() {
 
     ofstream dataOut;
-
-    
     dataOut.open("outputfile.txt");
 
     // TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size)
@@ -38,11 +37,17 @@ int main() {
     readTemperatures(monthTemp, size);
 
     // TODO: Step 4 - Print the temperatures
-    cout << "Day Temp" << endl;
-    printTemperatures(monthTemp, size);
+    dataOut << "Day Temp" << endl;
+    printTemperatures(dataOut, monthTemp, size);
 
     // TODO: Step 5 - Compute and display min, max, and average temperature
+    TemperatureRecord minTemp = findMin(monthTemp, size);
+    TemperatureRecord maxTemp = findMax(monthTemp, size);
+    double avgTemp = findAverage(monthTemp, size);
 
+    dataOut << "on day " << minTemp.day << " Minimum Temperature: " << minTemp.temperature << endl;
+    dataOut << "on day " << maxTemp.day << " Maximum Temperature: " << maxTemp.temperature << endl;
+    dataOut << "Average Temperature: " << fixed << showpoint << setprecision(2) << avgTemp << endl;
 
     dataOut.close();  
 
@@ -56,9 +61,7 @@ void readTemperatures(TemperatureRecord monthTemp[], int &size)
     ifstream dataIn;
     dataIn.open("temps.txt");
 
-    TemperatureRecord monthTemp[MAX_DAYS];
-
-    while (size < MAX_DAYS && dataIn >> monthTemp[MAX_DAYS].day >> monthTemp[MAX_DAYS].temperature)
+    while (size < MAX_DAYS && dataIn >> monthTemp[size].day >> monthTemp[size].temperature)
     {
         size++;
     }
@@ -68,24 +71,55 @@ void readTemperatures(TemperatureRecord monthTemp[], int &size)
 
 // TODO: Step 7 - Implement printTemperatures()
 // Print all stored temperatures in a formatted table
-void printTemperatures(const TemperatureRecord monthTemp[], int size)
+void printTemperatures(ofstream& dataOut, const TemperatureRecord monthTemp[], int size)
 {
     for (int position = 0; position < size; position++)
     {
-        cout << monthTemp[position].day << " " << monthTemp[position].temperature << endl;
+        dataOut << monthTemp[position].day << " " << monthTemp[position].temperature << endl;
     }
 } 
 
 
 // TODO: Step 8 - Implement findMin()
 // Return the TemperatureRecord with the lowest temperature
-
+TemperatureRecord findMin(const TemperatureRecord monthTemp[], int size)
+{
+    TemperatureRecord minTemp = monthTemp[0];
+    for (int position = 1; position < size; position++)
+    {
+        if (monthTemp[position].temperature < minTemp.temperature)
+        {
+            minTemp = monthTemp[position];
+        }
+    }
+    return minTemp;
+}
 
 
 // TODO: Step 9 - Implement findMax()
 // Return the TemperatureRecord with the highest temperature
-
+TemperatureRecord findMax(const TemperatureRecord monthTemp[], int size)
+{
+    TemperatureRecord maxTemp = monthTemp[0];
+    for (int position = 1; position < size; position++)
+    {
+        if (monthTemp[position].temperature > maxTemp.temperature)
+        {
+            maxTemp = monthTemp[position];
+        }
+    }
+    return maxTemp;
+}
 
 
 // TODO: Step 10 - Implement findAverage()
 // Compute and return the average temperature
+double findAverage(const TemperatureRecord monthTemp[], int size)
+{
+    double sum = 0;
+    for (int position = 0; position < size; position++)
+    {
+        sum += monthTemp[position].temperature;
+    }
+    return sum / size;
+}
